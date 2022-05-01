5288a2a56bfb4cc74ad4cd186537676525d86b1b
ario21 <hosein.iup93@gmail.com>
2022-05-01 13:12:14 +0430

    End of Part4 in 01/05/2022

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,11 +7,35 @@ for(var i=0; i < updateBtns.length; i++){
         console.log('productId:', productId, 'action:', action)
         console.log('USER:', user)
         if(user === 'AnonymousUser'){
-            console.log('Not logged in')
+            addCookeiItem(productId, action)
         }else{
             updateUserOrder(productId, action)
         }
     })
+}
+
+function addCookeiItem(productId, action){
+    console.log('Not logged in..')
+
+    if (action == 'add'){
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0){
+            console.log('remove Item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action){
